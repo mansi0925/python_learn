fa76acd20b49f8b62605451188676bde9bba1651
mansi0925 <147995046+mansi0925@users.noreply.github.com>
2023-12-31 14:30:54 +0530

    Indentation

---
+++ 7 days of python/Indentation.py
@@ -0,0 +1,14 @@
+"""A specific distance that we have to manage while writing python code.
+We do not use curly braces in puthon, insead we use indentation to seperate
+specific block of code from each other."""
+
+a = 23
+b = 12
+if a > b:
+    print("A is greater than B")
+    print("more lines")
+else:
+    print("A is not greater than B")
+    print("2nd line in else")
+
+print("Out of code block")
